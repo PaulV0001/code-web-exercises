9aa5f0dde40efdc442e66870ddf7e4880b3433f3
PaulV0001 <valdezpaul00@gmail.com>
2017-08-17 10:14:37 -0500

    Added code for a multiplication table for a given number

---
+++ js/for_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var userQuestion = prompt("Choose a number between 1-10");
+var j;
+
+for (var i = 1; i <= 10; i++) {
+    console.log(userQuestion + "x" + i + "=" + i * userQuestion);
+
+    }
+
