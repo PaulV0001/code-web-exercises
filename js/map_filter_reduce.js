8404dd4d517d5d743f02b6f455f7286ae8b09352
paulvaldez <valdezpaul00@gmail.com>
2017-09-11 16:47:58 -0500

    Completed the map, filter and reduce exercise.

---
+++ js/map_filter_reduce.js
@@ -0,0 +1,47 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+
+    let totalLanguages = users.filter(user => user.languages.length >= 3);
+    console.log(totalLanguages);
+
+    let totalEmails = users.map(user => user.email);
+    console.log(totalEmails);
+
+    let newObj = users.reduce((tempObj, user) => {
+        tempObj[user.id] = user;
+        return tempObj;
+    }, {});
+    console.log(newObj);
+
